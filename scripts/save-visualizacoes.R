a4cf33f4d4026365c41077bd92110d055891c304
Felipe Figueiredo <philsf79@gmail.com>
2021-09-20 20:41:04 -0300

    opções de resolução para publicação

--- scripts/save-visualizacoes.R
+++ scripts/save-visualizacoes.R
@@ -10,3 +10,19 @@ ggsave("figures/esp_sec_corr.png", esp.sec.corr, h=height, w=width, units = "cm"
 ggsave("figures/esp_sec_musc.png", esp.sec.musc, h=height, w=width, units = "cm")
 ggsave("figures/desc_movim.png", desc.movim, h=height, w=width, units = "cm")
 ggsave("figures/desc_local.png", desc.local, h=height, w=width, units = "cm")
+
+# resoluções de publicação ------------------------------------------------
+
+ggsave("figures/esp_prev_1col.png", esp.prev, h=3.5, w=3.5)
+ggsave("figures/esp_sec_fut_1col.png", esp.sec.fut, h=3.5, w=3.5)
+ggsave("figures/esp_sec_corr_1col.png", esp.sec.corr, h=3.5, w=3.5)
+ggsave("figures/esp_sec_musc_1col.png", esp.sec.musc, h=3.5, w=3.5)
+ggsave("figures/desc_movim_1col.png", desc.movim, h=3.5, w=3.5)
+ggsave("figures/desc_local_1col.png", desc.local, h=3.5, w=3.5)
+
+ggsave("figures/esp_prev_2col.png", esp.prev, h=3.5, w=7)
+ggsave("figures/esp_sec_fut_2col.png", esp.sec.fut, h=3.5, w=7)
+ggsave("figures/esp_sec_corr_2col.png", esp.sec.corr, h=3.5, w=7)
+ggsave("figures/esp_sec_musc_2col.png", esp.sec.musc, h=3.5, w=7)
+ggsave("figures/desc_movim_2col.png", desc.movim, h=3.5, w=7)
+ggsave("figures/desc_local_2col.png", desc.local, h=3.5, w=7)
