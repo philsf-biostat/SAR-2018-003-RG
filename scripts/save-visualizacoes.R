c1fdd9397c69d83044676be298df60f6920162c9
Felipe Figueiredo <philsf79@gmail.com>
2021-09-21 13:39:24 -0300

    figuras reduzidas para caber na página

--- scripts/save-visualizacoes.R
+++ scripts/save-visualizacoes.R
@@ -8,8 +8,8 @@ ggsave("figures/esp_prev.png", esp.prev, h=height, w=10, units = "cm")
 ggsave("figures/esp_sec_fut.png", esp.sec.fut, h=height, w=width, units = "cm")
 ggsave("figures/esp_sec_corr.png", esp.sec.corr, h=height, w=width, units = "cm")
 ggsave("figures/esp_sec_musc.png", esp.sec.musc, h=height, w=width, units = "cm")
-ggsave("figures/desc_movim.png", desc.movim, h=height, w=10, units = "cm")
-ggsave("figures/desc_local.png", desc.local, h=height, w=10, units = "cm")
+ggsave("figures/desc_movim.png", desc.movim, h=8, w=10, units = "cm")
+ggsave("figures/desc_local.png", desc.local, h=8, w=10, units = "cm")
 
 # resoluções de publicação ------------------------------------------------
 
