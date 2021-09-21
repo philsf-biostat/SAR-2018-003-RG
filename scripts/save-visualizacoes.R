5b38f715c60e149c17033c06b783ffa5d6341afa
Felipe Figueiredo <philsf79@gmail.com>
2021-09-21 12:28:54 -0300

    figuras redefinidas (best practices)
    - tamanhos ajustados à largura máxima para publicação (7in)
    - elementos textuais reposicionados para melhor visualização
    - tema de cores aplicado consistentemente a todos os gráficos
    - labels de % sem decimais (clutter)
    - gráficos de atividades secundárias replanejados (tamanho) para painel A/B/C

--- scripts/save-visualizacoes.R
+++ scripts/save-visualizacoes.R
@@ -1,28 +1,28 @@
 source("scripts/visualizacoes.R", encoding = 'UTF-8')
 
-height <- 8
+height <- 12
 width <- 8
 units <- "cm"
 
-ggsave("figures/esp_prev.png", esp.prev, h=height, w=width, units = "cm")
+ggsave("figures/esp_prev.png", esp.prev, h=height, w=10, units = "cm")
 ggsave("figures/esp_sec_fut.png", esp.sec.fut, h=height, w=width, units = "cm")
 ggsave("figures/esp_sec_corr.png", esp.sec.corr, h=height, w=width, units = "cm")
 ggsave("figures/esp_sec_musc.png", esp.sec.musc, h=height, w=width, units = "cm")
-ggsave("figures/desc_movim.png", desc.movim, h=height, w=width, units = "cm")
-ggsave("figures/desc_local.png", desc.local, h=height, w=width, units = "cm")
+ggsave("figures/desc_movim.png", desc.movim, h=height, w=10, units = "cm")
+ggsave("figures/desc_local.png", desc.local, h=height, w=10, units = "cm")
 
 # resoluções de publicação ------------------------------------------------
 
 ggsave("figures/esp_prev_1col.png", esp.prev, h=3.5, w=3.5)
-ggsave("figures/esp_sec_fut_1col.png", esp.sec.fut, h=3.5, w=3.5)
-ggsave("figures/esp_sec_corr_1col.png", esp.sec.corr, h=3.5, w=3.5)
-ggsave("figures/esp_sec_musc_1col.png", esp.sec.musc, h=3.5, w=3.5)
+ggsave("figures/esp_sec_fut_1col.png",  esp.sec.fut,  h=4, w=2.3) # w=2.3 para juntar em 3 painéis = 7
+ggsave("figures/esp_sec_corr_1col.png", esp.sec.corr, h=4, w=2.3) # w=2.3 para juntar em 3 painéis = 7
+ggsave("figures/esp_sec_musc_1col.png", esp.sec.musc, h=4, w=2.3) # w=2.3 para juntar em 3 painéis = 7
 ggsave("figures/desc_movim_1col.png", desc.movim, h=3.5, w=3.5)
 ggsave("figures/desc_local_1col.png", desc.local, h=3.5, w=3.5)
 
 ggsave("figures/esp_prev_2col.png", esp.prev, h=3.5, w=7)
-ggsave("figures/esp_sec_fut_2col.png", esp.sec.fut, h=3.5, w=7)
-ggsave("figures/esp_sec_corr_2col.png", esp.sec.corr, h=3.5, w=7)
-ggsave("figures/esp_sec_musc_2col.png", esp.sec.musc, h=3.5, w=7)
+ggsave("figures/esp_sec_fut_2col.png",  esp.sec.fut,  h=4, w=7)
+ggsave("figures/esp_sec_corr_2col.png", esp.sec.corr, h=4, w=7)
+ggsave("figures/esp_sec_musc_2col.png", esp.sec.musc, h=4, w=7)
 ggsave("figures/desc_movim_2col.png", desc.movim, h=3.5, w=7)
 ggsave("figures/desc_local_2col.png", desc.local, h=3.5, w=7)
--- scripts/visualizacoes.R
+++ scripts/visualizacoes.R
@@ -33,8 +33,8 @@ theme_update(
 
 # ordem de prevalência (Pareto)
 esp.prev <- ggplot(esportes, aes(reorder(ESPORTE, ESPORTE, length), fill = PRINCIPAL)) +
-  geom_bar(fill = ff.col) +
-  xlab("Esporte") + ylab("") +
+  geom_bar() +
+  labs(x = "Esporte principal", y = "") +
   # scale_color_brewer(palette = ff.pal) +
   scale_fill_brewer(palette = ff.pal) +
   coord_flip() +
@@ -56,30 +56,30 @@ ids.musculacao <- as.numeric(levels(ids.musculacao)[ids.musculacao])
 
 esp.sec.fut <- ggplot(esportes[ID %in% ids.futebol & PRINCIPAL == "Secundário"], aes(reorder(ESPORTE, ESPORTE, length))) +
   geom_bar(fill = ff.col) +
-  xlab("Esporte") + ylab("") +
+  labs(x = "Esporte acessório quando o principal é futebol", y = "") +
   # scale_color_brewer(palette = ff.pal) +
   scale_fill_brewer(palette = ff.pal) +
   ylim(c(0, 40)) +
   coord_flip()+
-  ggtitle("Esporte acessório quando o principal é futebol")
+  ggtitle("")
 
 esp.sec.corr <- ggplot(esportes[ID %in% ids.corrida & PRINCIPAL == "Secundário"], aes(reorder(ESPORTE, ESPORTE, length))) +
   geom_bar(fill = ff.col) +
-  xlab("Esporte") + ylab("") +
+  labs(x = "Esporte acessório quando o principal é corrida", y = "") +
   ylim(c(0, 40)) +
   # scale_color_brewer(palette = ff.pal) +
   scale_fill_brewer(palette = ff.pal) +
   coord_flip()+
-  ggtitle("Esporte acessório quando o principal é corrida")
+  ggtitle("")
 
 esp.sec.musc <- ggplot(esportes[ID %in% ids.musculacao & PRINCIPAL == "Secundário"], aes(reorder(ESPORTE, ESPORTE, length))) +
   geom_bar(fill = ff.col) +
-  xlab("Esporte") + ylab("") +
+  labs(x = "Esporte acessório quando o principal é musculação", y = "") +
   ylim(c(0, 40)) +
   # scale_color_brewer(palette = ff.pal) +
   scale_fill_brewer(palette = ff.pal) +
   coord_flip()+
-  ggtitle("Esporte acessório quando o principal é musculação")
+  ggtitle("")
 
 desc.idade <- ggplot(participantes, aes(IDADE.cat)) +
   geom_bar(fill = ff.col) +
@@ -118,7 +118,7 @@ desc.sexo <- ggplot(participantes, aes(SEXO)) +
 
 desc.movim <- ggplot(movim, aes(reorder(MOVIMENTO, MOVIMENTO, length))) +
   geom_bar(aes(y = (..count..)/sum(..count..)), fill = ff.col) +
-  scale_y_continuous(labels=percent) +
+  scale_y_continuous(labels=label_percent(accuracy=1), limits = c(0, .4)) +
   # scale_color_brewer(palette = ff.pal) +
   scale_fill_brewer(palette = ff.pal) +
   xlab("Movimento em que sente dor") + ylab("") +
@@ -127,7 +127,7 @@ desc.movim <- ggplot(movim, aes(reorder(MOVIMENTO, MOVIMENTO, length))) +
 
 desc.local <- ggplot(locais, aes(reorder(LOCAL, LOCAL, length))) +
   geom_bar(aes(y = (..count..)/sum(..count..)), fill = ff.col) +
-  scale_y_continuous(labels=percent) +
+  scale_y_continuous(labels=label_percent(accuracy=1), limits = c(0, .4)) +
   # scale_color_brewer(palette = ff.pal) +
   scale_fill_brewer(palette = ff.pal) +
   xlab("Local em que sente dor") + ylab("") +
